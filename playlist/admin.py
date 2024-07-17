2cee02cd21f31bea152039144be74d70140bbe2e
Rahmatjon <rahmatjonibragimov17@gmail.com>
2024-07-17 17:57:11 +0500

    add: added videomodel

--- playlist/admin.py
+++ playlist/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import VideoModel
+
 
 # Register your models here.
+class VideoModelAdmin(admin.ModelAdmin):
+  list_display = ('name', 'joined_date', 'video')
+  prepopulated_fields = {"slug": ("name",)}
+  
+admin.site.register(VideoModel, VideoModelAdmin)
