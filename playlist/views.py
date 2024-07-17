011bc76b0ae88c3515f3775b04318a2e785189d5
Rahmatjon <rahmatjonibragimov17@gmail.com>
2024-07-17 17:57:51 +0500

    add: added detailview

--- playlist/views.py
+++ playlist/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic.detail import DetailView
 
 # Create your views here.
+def home(req):
+    return render(req, 'playlist/index.html', {'salom': 'salaomaaaa'})
+
+
+from playlist.models import VideoModel
+
+
+def HomeViews(request):
+    video=VideoModel.objects.all()
+    return render(request, 'playlist/index.html', {'video': video})
+
+
+class GeeksDetailView(DetailView):
+    model = VideoModel
+    template_name = 'playlist/detail.html'
+    context_object_name = 'video'
