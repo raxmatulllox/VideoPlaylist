8cd3ac44d71bafbff6e223693830a0a2ec8549a8
Rahmatjon <rahmatjonibragimov17@gmail.com>
2024-08-22 19:58:43 +0500

    add venv files

--- playlist/models.py
+++ playlist/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from django.template.defaultfilters import slugify  # new
+from django.template.defaultfilters import slugify
 from django.urls import reverse
 
 
 # Create your models here.
 class VideoModel(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     video = models.FileField(upload_to='videos/')
     slug = models.SlugField(null=False, unique=True)
     joined_date = models.DateTimeField(auto_now=True)
@@ -14,10 +14,11 @@ class VideoModel(models.Model):
     def get_absolute_url(self):
         return reverse("article_detail", kwargs={"slug": self.slug})
 
-    def save(self, *args, **kwargs):  # new
+    def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(self.title)
+            self.slug = slugify(self.name)
         return super().save(*args, **kwargs)
     
     def __str__(self) -> str:
         return self.name
+    
