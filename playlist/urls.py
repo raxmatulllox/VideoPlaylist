0a2641b26c7e455bc541ac7ee9f125a6d5f0d96f
Rahmatjon <rahmatjonibragimov17@gmail.com>
2024-08-22 19:59:41 +0500

    update urls

--- playlist/urls.py
+++ playlist/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import home, HomeViews, GeeksDetailView
+from .views import *
 
 app_name = 'playlist'
 
 urlpatterns = [
-    path('', HomeViews),
-    path('<slug:slug>/', GeeksDetailView.as_view(), name='detail'),
+    path('', HomeViews, name='home'),
+    path('video-down/', VideoPostView.as_view(), name='video-download-page'),
+    # path('download/', youtube, name='download_video'),
+    path('youtube/', home.as_view(),name="home-dev"),
+    path('<slug:slug>/', VideoDetailView.as_view(), name='detail'),
 ]
